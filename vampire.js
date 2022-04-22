c29e0d047f603c3e3c3ad1668c38bdd0ba164420
Charles Ngo <charlesngo37@gmail.com>
2022-04-22 16:18:39 -0700

    added vampire functions still working on stretch goal

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
+    return;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let counter = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      counter++;
+    }
+    return counter;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const thisAge = this.numberOfVampiresFromOriginal;
+    const otherAge = vampire.numberOfVampiresFromOriginal;
 
+    return thisAge < otherAge;
   }
 
   /** Stretch **/
@@ -36,8 +48,30 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire = this;
+    let output = null;
 
+    if (currentVampire.creator === null) {
+      return currentVampire;
+    }
+    if (vampire.creator === null) {
+      return vampire;
+    }
+
+    if (currentVampire.numberOfVampiresFromOriginal === 1) {
+      return currentVampire.creator;
+    }
+
+    if (vampire.numberOfVampiresFromOriginal === 1) {
+      return vampire.creator;
+    }
+   
+    return output;
   }
+
+
+
+
 }
 
 module.exports = Vampire;
